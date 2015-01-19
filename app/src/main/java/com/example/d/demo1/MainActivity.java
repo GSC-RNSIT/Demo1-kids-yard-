c38f033ce7685a01727942f51ac3b85846f05d6a
ShamreenM <noormohd276@gmail.com>
2015-01-19 13:19:23 +0530

    Assignment 1 (Android app development )
    App to create a button and display "you just typed &input ".

---
+++ app/src/main/java/com/example/d/demo1/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.d.demo1;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+    Button mButton;
+    EditText mEdit;
+    TextView mText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mButton = (Button)findViewById(R.id.button1);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                mEdit   = (EditText)findViewById(R.id.editText);
+                mText = (TextView)findViewById(R.id.textView1);
+                mText.setText("You have just typed "+mEdit.getText().toString()+"!");
+            }
+        });
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
